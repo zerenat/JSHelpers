f7515051cb30bf767eaddab18a9dbdada41ff268
Martin <m.hein090@gmail.com>
2023-01-15 11:08:23 +0000

    finished webhook logic

--- chime_webhook_with_tm.js
+++ chime_webhook_with_tm.js
@@ -1,4 +1,19 @@
 function sendWebhook(targetUrl, payload){
     return new Promise(function(resolve, reject) {
+        GM.xmlHttpRequest({
+            method: "POST",
+            url: targetUrl,
+            headers: {"Content-Type": "application/json"},
+            data: JSON.stringify({'Content': payload}),
+            onload: function(response){
+                let status = response.status
+                if (status === 200){
+                    resolve(status)
+                } else {
+                    console.log('Request failed with status: ' + status)
+                    reject(status)
+                }
+            }
+        })
     });
 }
