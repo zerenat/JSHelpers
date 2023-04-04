300e563084ee068a70fb8590a4216dfffefe8460
Martin <m.hein090@gmail.com>
2023-04-04 15:30:55 +0100

    Added sound alerts, modified logic

--- modification_tracker.js
+++ modification_tracker.js
@@ -1,61 +1,98 @@
 // ==UserScript==
-// @name         ModificationTracker
+// @name         TableChangeTracker
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  A system to play notification sound on incoming WAT Task
-// @author       Martin Hein - heimarti
-// @match        
+// @description  A system to track new table items
+// @author       Martin Hein
+// @match        ""
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
-const modificationTracker = {
-    trackedElement: undefined,
-    firstCheck: true,
-    elementState: undefined,
-    getTrackedElement: ()=>{
-        return this.trackedElement;
+const changeTracker = {
+    tableData: {'tableElement': undefined,
+                'tableRows': 0,
+                'changeCounter': 0,
+                'identificationStrings': [],
+               },
+    get getTableData(){
+        return this.tableData;
     },
-    setTrackedElement: (value)=>{
-        this.trackedElement = value;
+    set setTableData(value){
+        this.tableData = value;
     },
-    getElementState: ()=>{
-        return this.elementState;
+    get getTableElement(){
+        console.log("Retrieving tracked element")
+        return this.tableData.tableElement;
     },
-    setElementState: (value)=>{
-        this.elementState = value;
+    set setTableElement(value){
+        console.log("Setting tracked element to: ", value)
+        this.tableData.tableElement = value;
     },
-    checkForModifications: ()=>{
-        if (this.firstCheck){
-            this.firstCheck = false;
-            this.elementState = this.trackedElement.innerText;
-            return false;
-        } else{
-            if (this.elementState != this.trackedElement.innertText){
-                this.elementState = this.trackedElement.innertText;
-                return true;
-            } else{
-                return false;
+    get getTableRows(){
+        console.log("retrieving element state")
+        return this.tableData.tableRows;
+    },
+    set setTableRows(value){
+        console.log("Setting element state to: ", value)
+        this.tableData.tableRows = value;
+    },
+    get getChangeCounter(){
+        return this.tableData.changeCounter;
+    },
+    set setChangeCounter(value){
+        this.tableData.changeCounter = value;
+    },
+    get getIdentificationStrings(){
+        return this.tableData.identificationStrings;
+    },
+    set setIdentificationStrings(value){
+        this.tableData.identificationStrings = value;
+    },
+    incrementChangeCounter: function(){
+        this.tableData.changeCounter += 1;
+    },
+    checkForChange: function(){
+        let tableRowIds = []
+        let tableChange = false;
+        for (let row of this.tableData.tableElement.childNodes){
+            let rowIdString = row.innerText.replace(/[\n\t_ ]/gm, "").split(/(\s)/)[0]
+            tableRowIds.push(rowIdString);
+            if (!this.tableData.identificationStrings.includes(rowIdString)){
+                tableChange = true;
             }
         }
-
-    },
+        this.tableData.identificationStrings = tableRowIds;
+        if(tableChange){
+            if (this.tableData.changeCounter > 0){
+                return true;
+            } else {
+                this.incrementChangeCounter();
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }
 
-const alertSound= {
-    playAlertSound: ()=>{
-        console.log("playing alert sound")
+const audioAlerts = {
+    playerElement: document.createElement('audio'),
+    soundFileNewTask: "",
+    playNotificationNewTask: function (){
+        this.playerElement.src = this.soundFileNewTask;
+        this.playerElement.preload = 'auto';
+        this.playerElement.play()
     },
 }
 
 const main = ()=>{
-    console.log('starting script')
-    modificationTracker.setTrackedElement = document.getElementById("Tasks-Editor").childNodes[1].childNodes[0].childNodes[0]
-    while (true){
-        setTimeout(()=>{
-            modificationTracker.checkForModifications();
-        },500);
-    }
+    changeTracker.setTableElement = document.getElementById('Tasks-Editor').children[1].children[0].children[0]
+    setInterval(()=>{
+        if (changeTracker.checkForChange()){
+            audioAlerts.playNotificationNewTask();
+        }
+    }, 2000);
 };
 
 main();
